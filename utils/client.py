e931ee1eeda004052743d3aefbba4090e0fa659d
Jiwonii97 <wldnjs3633@naver.com>
2022-10-28 19:36:53 +0900

    함수 호출 방식 상수화 진행

--- utils/client.py
+++ utils/client.py
@@ -1,17 +1,19 @@
 import json
 import boto3
 
+from constant import KEY
 
-def getClientBucket(keyPath, readMode):
+
+def getClientBucket():
     # go to Keys and get dictionary
-    with open(keyPath, readMode, encoding="utf-8") as f:
+    with open(KEY.KEY_PATH, KEY.READ_MODE, encoding=KEY.UTF8) as f:
         keys = json.load(f)
 
-    accessKeyId = keys["aws_access_key_id"]
-    secretAccessKey = keys["aws_secret_access_key"]
-    storageBucketName = keys["aws_storage_bucket_name"]
+    accessKeyId = keys[KEY.AWS_ACCESS_KEY_ID]
+    secretAccessKey = keys[KEY.AWS_SECRET_ACCESS_KEY]
+    storageBucketName = keys[KEY.AWS_STORAGE_BUCKET_NAME]
 
-    s3Client = boto3.client("s3", aws_access_key_id=accessKeyId,
+    s3Client = boto3.client(KEY.S3, aws_access_key_id=accessKeyId,
                             aws_secret_access_key=secretAccessKey)
 
     return s3Client, storageBucketName
