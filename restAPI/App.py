9ee8a067de5e6bcc5706d8a403736dd6365aeac8
Jiwonii97 <wldnjs3633@naver.com>
2022-10-28 19:36:45 +0900

    변수 상수 처리

--- restAPI/App.py
+++ restAPI/App.py
@@ -1,12 +1,10 @@
-from utils.client import getClientBucket
 from constant import KEY
-
 # request 데이터를 가지고 있는 클래스
 
 
 class App():
     def __init__(self, app):
-        self.client, self.bucket = getClientBucket(KEY.KEY_PATH, KEY.READ_MODE)
+        self.client, self.bucket = KEY.S3CLIENT, KEY.S3BUCKET
         [file, keyName, action] = app
         self.keyName = keyName
         self.file = file
