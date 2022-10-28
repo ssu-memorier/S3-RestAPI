668881b1d35f5b148cb814b6c054bbe89550c8f6
Jiwonii97 <wldnjs3633@naver.com>
2022-10-28 11:01:29 +0900

    client, bucket 키값 상수화 진행

--- constant/KEY.py
+++ constant/KEY.py
@@ -1,10 +1,7 @@
+from utils import client
+
 KEY_PATH = "assets/aws_keys.json"
 READ_MODE = "r"
 UTF8 = "utf-8"
 
-S3 = "s3"
-
-# AWS Key's key
-AWS_ACCESS_KEY_ID = "aws_access_key_id"
-AWS_SECRET_ACCESS_KEY = "aws_secret_access_key"
-AWS_STORAGE_BUCKET_NAME = "aws_storage_bucket_name"
+S3CLIENT, S3BUCKET = client.getClientBucket(KEY_PATH, READ_MODE)
