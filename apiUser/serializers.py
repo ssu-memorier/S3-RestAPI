b60daaa4dc3aa54aec0e004c9a8ef2d39043f992
Jiwonii97 <wldnjs3633@naver.com>
2022-11-02 00:46:56 +0900

    Request 시리얼라이즈 생성

---
+++ apiUser/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+from rest_framework import status
+
+'''
+    django REST framework에서 제공하는 serializer를 활용하여
+    request로 받은 data를 역직렬화 하여 DB에 반영하고
+    reponse로 사용 될 data를 다시 직렬화하여 json이나 xml등으로 손쉽게 변환이 가능하다.
+'''
+
+
+class FileSerializer(serializers.Serializer):       # file object
+    uid = serializers.CharField(max_length=128)
+    keyName = serializers.CharField(max_length=1024)
+
+    def errors(self):
+        return status.HTTP_400_BAD_REQUEST
+
+
+class ListSerializer(serializers.Serializer):       # list object
+    uid = serializers.CharField(max_length=128)
+
+    def errors(self):
+        return status.HTTP_400_BAD_REQUEST
