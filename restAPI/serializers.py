d21c12f3130307d4352f9f1207efb3942c79db32
Jiwonii97 <wldnjs3633@naver.com>
2022-11-02 00:44:42 +0900

    이전의 불필요한 요소 삭제

--- restAPI/serializers.py
+++
@@ -1,9 +0,0 @@
-from constant import REQUEST
-
-
-def request(requestFile):
-    file = requestFile[REQUEST.FILE]
-    keyName = requestFile[REQUEST.KEYNAME]
-    action = requestFile[REQUEST.ACTION]
-
-    return [file, keyName, action]
