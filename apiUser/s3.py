7b6d534226b56ae2a03cb0d58b48425079aaa814
Jiwonii97 <wldnjs3633@naver.com>
2022-11-02 01:09:28 +0900

    함수명 일부 수정

--- apiUser/s3.py
+++ apiUser/s3.py
@@ -3,38 +3,38 @@ from utils.client import getClientBucket
 s3Client, s3Bucket = getClientBucket()      # 서버 클라이언트, 버킷 정보
 
 
-def putObject(keyname, data):
-    s3Client.upload_fileobj(data, s3Bucket, keyname)
+def putObject(keyName, data):
+    s3Client.upload_fileobj(data, s3Bucket, keyName)
 
 
-def getObject(uid, keyname):
+def getObject(uid, keyName):
     contents = [data['Key'] for data in getList(uid)]
     try:
         # 해당 파일이 없는 경우 에러
-        if keyname not in contents:
+        if keyName not in contents:
             raise KeyError
         return s3Client.get_object(
-            Bucket=s3Bucket, Key=keyname)['Body'].read()
+            Bucket=s3Bucket, Key=keyName)['Body'].read()
     except KeyError:
         return None
 
 
-def deleteObject(uid, keyname):
+def deleteObject(uid, keyName):
     contents = [data['Key'] for data in getList(uid)]
     try:
         # 해당 파일이 없는 경우 에러
-        if keyname not in contents:
+        if keyName not in contents:
             raise KeyError
-        s3Client.delete_object(Bucket=s3Bucket, Key=keyname)
+        s3Client.delete_object(Bucket=s3Bucket, Key=keyName)
 
         return True
     except KeyError:
         return False
 
 
-def getList(userId):
+def getList(uid):
     try:
-        return s3Client.list_objects_v2(Bucket=s3Bucket, Prefix=userId)[
+        return s3Client.list_objects_v2(Bucket=s3Bucket, Prefix=uid)[
             'Contents']       # 해당 userID를 가진 컨텐츠만 가져옴
     except KeyError:
         return None
