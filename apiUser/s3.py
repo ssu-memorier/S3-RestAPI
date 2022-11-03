b1e5ca823f013ed738ce23f6a69ad9842f727649
Jiwonii97 <wldnjs3633@naver.com>
2022-11-04 00:03:16 +0900

    일부 코드 내용 수정

--- apiUser/s3.py
+++ apiUser/s3.py
@@ -6,30 +6,37 @@ s3Client, s3Bucket = getClientBucket()      # 서버 클라이언트, 버킷 정
 
 def createObject(uid, keyName, data):
     contents = [data['Key'] for data in getList(uid)]
+
     try:
         # 해당 파일이 없는 경우 에러
         if keyName in contents:
             raise KeyError
         s3Client.upload_fileobj(data, s3Bucket, keyName)
+
         return status.HTTP_200_OK
+
     except KeyError:
         return status.HTTP_400_BAD_REQUEST
 
 
 def getObject(uid, keyName):
     contents = [data['Key'] for data in getList(uid)]
+
     try:
         # 해당 파일이 없는 경우 에러
         if keyName not in contents:
             raise KeyError
+
         return s3Client.get_object(
             Bucket=s3Bucket, Key=keyName)['Body'].read()
+
     except KeyError:
         return None
 
 
 def deleteObject(uid, keyName):
     contents = [data['Key'] for data in getList(uid)]
+
     try:
         # 해당 파일이 없는 경우 에러
         if keyName not in contents:
@@ -37,6 +44,7 @@ def deleteObject(uid, keyName):
         s3Client.delete_object(Bucket=s3Bucket, Key=keyName)
 
         return True
+
     except KeyError:
         return False
 
@@ -45,5 +53,6 @@ def getList(uid):
     try:
         return s3Client.list_objects_v2(Bucket=s3Bucket, Prefix=uid)[
             'Contents']       # 해당 userID를 가진 컨텐츠만 가져옴
-    except KeyError:
+
+    except KeyError:        # 유저 정보가 없으면 에러 발생
         return None
