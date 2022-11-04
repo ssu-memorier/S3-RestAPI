379a42aa6a27c402ade46635d136230ee4d5b1c1
Jiwonii97 <wldnjs3633@naver.com>
2022-11-04 10:49:37 +0900

    cors 관련 내용 추가

--- djangoRestAPI/settings.py
+++ djangoRestAPI/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'apiUser.apps.ApiUserConfig',
     'rest_framework',
+    'corsheaders',  # CORS 관련 추가
 ]
 
 MIDDLEWARE = [
@@ -54,6 +55,17 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',     # CORS 관련 추가
+]
+
+CORS_ORIGIN_WHITELIST = ['http://0.0.0.0:8080', 'http://localhost:8080',
+                         'http://127.0.0.1:3000', 'http://localhost:3000',
+                         "https://gomgukstorage.paas-ta.org"]
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_METHODS = [
+    "DELETE",
+    "GET",
+    "POST",
 ]
 
 ROOT_URLCONF = 'djangoRestAPI.urls'
