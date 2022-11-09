d73544caa325952f58b3a35535a38a45b25f5334
Jiwonii97 <wldnjs3633@naver.com>
2022-11-09 18:31:09 +0900

    일부 상수 추가

--- constants/REQUEST.py
+++ constants/REQUEST.py
@@ -8,6 +8,10 @@ SUCCESS = True
 FAIL = False
 
 PDF = "application/pdf"
+ZIP = "application/zip"
+
+CONTENT_DISPOSTION = 'Content-Disposition'
+CONTENT_DISPOSTION_BODY = 'attachment; filename=data.zip'
 
 GET, POST, PUT, DELETE = "GET", "POST", "PUT", "DELETE"
 
