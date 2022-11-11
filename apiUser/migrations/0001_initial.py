e65ade2a529df0c97d84a9b31abf4779029ddd57
Jiwonii97 <wldnjs3633@naver.com>
2022-11-11 19:19:48 +0900

    주석 추가

--- apiUser/migrations/0001_initial.py
+++ apiUser/migrations/0001_initial.py
@@ -2,6 +2,11 @@
 
 from django.db import migrations, models
 
+'''
+#   모델을 적용할때 사용하는 init 파일입니다.
+#   배포 진행후 해당 부분 추가 수정 진행하겠습니다
+'''
+
 
 class Migration(migrations.Migration):
 
@@ -14,7 +19,8 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Content',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.BigAutoField(auto_created=True,
+                 primary_key=True, serialize=False, verbose_name='ID')),
                 ('uid', models.CharField(max_length=64)),
                 ('dir', models.CharField(blank=True, max_length=200)),
                 ('key', models.CharField(max_length=100)),
