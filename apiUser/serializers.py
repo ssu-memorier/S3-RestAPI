1b620bfe1f68721c6d095f1f58f4c4016a718a30
Jiwonii97 <wldnjs3633@naver.com>
2022-11-12 22:34:58 +0900

    상수화 진행

--- apiUser/serializers.py
+++ apiUser/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from constants import MODEL
 
 '''
     django REST framework에서 제공하는 serializer를 활용하여
@@ -8,10 +9,10 @@ from rest_framework import serializers
 
 
 class FileSerializer(serializers.Serializer):       # file object
-    uid = serializers.CharField(max_length=64)
-    dir = serializers.CharField(max_length=200, allow_blank=True)
-    key = serializers.CharField(max_length=100)
+    uid = serializers.CharField(max_length=MODEL.UID_LENGTH)
+    dir = serializers.CharField(max_length=MODEL.DIR_LENGTH, allow_blank=True)
+    key = serializers.CharField(max_length=MODEL.KEY_LENGTH)
 
 
 class ListSerializer(serializers.Serializer):       # list object
-    uid = serializers.CharField(max_length=64)
+    uid = serializers.CharField(max_length=MODEL.UID_LENGTH)
