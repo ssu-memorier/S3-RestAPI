41d7aa2f2b1a88ac8f1cd7481bf88ca7afe40a85
Jiwonii97 <wldnjs3633@naver.com>
2022-11-12 22:35:52 +0900

    일부 코드 수정 및 관심사 이동

--- apiUser/s3.py
+++ apiUser/s3.py
@@ -1,12 +1,15 @@
 from utils.client import getClientBucket
 from utils import converter
+from utils import elements
 from constants import REQUEST
 
 s3Client, s3Bucket = getClientBucket()      # 서버 클라이언트, 버킷 정보
 
 
 def createObject(uid, keyName, data):
-    contents = [data['Key'] for data in getListObject(uid)]
+    listObject = elements.getListObject(s3Client, s3Bucket, uid)
+    contents = elements.getContents(listObject)
+
     try:
         # 해당 파일이 있는 경우 에러
         if f"{keyName}.pdf" in contents:
@@ -24,7 +27,8 @@ def createObject(uid, keyName, data):
 
 
 def getObject(uid, keyName):
-    contents = [data['Key'] for data in getListObject(uid)]
+    listObject = elements.getListObject(s3Client, s3Bucket, uid)
+    contents = elements.getContents(listObject)
 
     try:
         # 해당 파일이 없는 경우 에러
@@ -43,7 +47,8 @@ def getObject(uid, keyName):
 
 
 def deleteObject(uid, keyName):
-    contents = [data['Key'] for data in getListObject(uid)]
+    listObject = elements.getListObject(s3Client, s3Bucket, uid)
+    contents = elements.getContents(listObject)
 
     try:
         # 해당 파일이 없는 경우 에러
@@ -71,15 +76,8 @@ def saveJson(keyName, data):
 
 def getList(uid):
     try:
-        contents = getListObject(uid)
+        contents = elements.getListObject(s3Client, s3Bucket, uid)
         return converter.contents2List(contents)
 
     except KeyError:        # 유저 정보가 없으면 에러 발생
         return None
-
-
-def getListObject(uid):  # 해당 userID를 가진 컨텐츠만 가져옴
-    try:
-        return s3Client.list_objects_v2(Bucket=s3Bucket, Prefix=uid)['Contents']
-    except KeyError:
-        return REQUEST.DEFAULT_OBJECT_LIST
