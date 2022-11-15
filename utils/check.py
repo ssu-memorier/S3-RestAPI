84e025c850f0625abd98da299e5f96bfa3b82318
Jiwonii97 <wldnjs3633@naver.com>
2022-11-15 21:02:25 +0900

    함수 구조 일부 수정

--- utils/check.py
+++ utils/check.py
@@ -1,13 +1,9 @@
 import re
+from utils.elements import getUserPdfFile
 
 
 def checkDuplicate(key, contents):
-    files = []
-    for content in contents:
-        if content.endswith('pdf'):
-            tokens = content.split('/')
-            keyName = tokens[-1][:-4]       # 파일 확장자 부분 잘라내기
-            files.append(keyName)
+    files = getUserPdfFile(contents)
 
     mainpattern = re.compile(f'{key}')      # 같은 파일을 찾는 용도
     subPattern = re.compile(f'^{key} \((\d)\)$')    # 중복되는 파일을 찾는 용도
@@ -15,17 +11,17 @@ def checkDuplicate(key, contents):
     files.sort()
     fileList = []   # 같은 이름을 갖고 있는 파일 리스트
     for file in files:
+        # 파일명이 같은 경우 -> 다른 이름으로 저장해야함
         if mainpattern.match(file):
-            for f in files:
-                if f == file:
-                    continue
-                if subPattern.match(f):
-                    fileList.append(f)
-            if not fileList:
-                return f"{key} (1)"
-            else:
-                break
-    else:   # 중복되는 파일이 없음
+            # 파일명이 같지 않으면서 '파일명 (숫자)' 형식을 가진 파일명만 추출
+            fileList = [f for f in files if f != file and subPattern.match(f)]
+
+            if fileList:
+                break    # '파일명 (숫자)' 형식의 파일이 있는 경우, 다음 스텝으로 진행
+
+            # '파일명 (숫자)' 형식의 파일이 없는 경우 -> 파일명 (1)로 생성
+            return f"{key} (1)"
+    else:   # 중복되는 파일이 없음 -> 새로 생성하면 됨
         return key
 
     maxNum = 0
