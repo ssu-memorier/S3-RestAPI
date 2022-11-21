0712a2b588e3a20d6ae3d35896baf183a9e84792
Jiwonii97 <wldnjs3633@naver.com>
2022-11-22 01:29:46 +0900

    중복 체크 로직 수정

--- utils/check.py
+++ utils/check.py
@@ -1,21 +1,17 @@
-import re
 from utils.elements import getPdfFileNames
 
 
 def checkDuplicate(key, contents):
     files = getPdfFileNames(contents)
-
-    mainpattern = re.compile(f'{key}')      # 같은 파일을 찾는 용도
-    subPattern = re.compile(f'^{key} \((\d)\)$')    # 중복되는 파일을 찾는 용도
-
     files.sort()
+
     duplicatedFileList = []   # 같은 이름을 갖고 있는 파일 리스트
     for file in files:
         # 파일명이 같은 경우 -> 다른 이름으로 저장해야함
-        if mainpattern.match(file):
+        if key == file:
             # 파일명이 같지 않으면서 '파일명 (숫자)' 형식을 가진 파일명만 추출
             duplicatedFileList = [
-                f for f in files if f != file and subPattern.match(f)]
+                f for f in files if f != file and f.startswith(key)]
 
             if duplicatedFileList:
                 break    # '파일명 (숫자)' 형식의 파일이 있는 경우, 다음 스텝으로 진행
