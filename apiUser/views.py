21d6b02e2563629964573bfca0cbc259b2ab7fd6
Jiwonii97 <wldnjs3633@naver.com>
2022-11-22 21:01:08 +0900

    상수 명 수정

--- apiUser/views.py
+++ apiUser/views.py
@@ -22,7 +22,7 @@ class FileViewSet(viewsets.ModelViewSet):
             pdfContent, jsonContent = getObject(uid, filePath)
 
             if pdfContent is None or jsonContent is None:     # 가져온 파일이 없는경우
-                return Response(MESSAGE.FILE_NOT_EXIST, status=status.HTTP_404_NOT_FOUND)
+                return Response(MESSAGE.FILE_DOES_NOT_EXIST, status=status.HTTP_404_NOT_FOUND)
 
             # set response
             response = HttpResponse(
@@ -41,7 +41,7 @@ class FileViewSet(viewsets.ModelViewSet):
 
     def create(self, request):
         if request.data[RQ.DATA].size > FILEMETA.LIMITED_FILESIZE:
-            return Response(MESSAGE.FILE_SIZE_EXCESS, status=status.HTTP_400_BAD_REQUEST)
+            return Response(MESSAGE.FILE_SIZE_EXCEEDED, status=status.HTTP_400_BAD_REQUEST)
 
         fileSerializer = FileSerializer(data=request.fileMeta)
 
@@ -98,7 +98,7 @@ class ListViewSet(viewsets.ModelViewSet):
             contents = getList(uid)
 
             if contents is None:       # Content가 없으면
-                return Response(MESSAGE.USER_INFO_NOT_EXIST, status=status.HTTP_404_NOT_FOUND)
+                return Response(MESSAGE.USER_INFO_IS_NOT_EXIST, status=status.HTTP_404_NOT_FOUND)
 
             return JsonResponse({RP.UID: uid, RP.CONTENTS: contents}, status=status.HTTP_200_OK)
         else:
