8bd30b1ee82ffbf492024b72b9ed435219224b4a
Jiwonii97 <wldnjs3633@naver.com>
2022-11-23 21:48:10 +0900

    AUTH 측에 JWT 토큰의 유효성 검사 진행

--- djangoRestAPI/middleware.py
+++ djangoRestAPI/middleware.py
@@ -2,7 +2,9 @@ from utils import elements, converter
 from constants import REQUEST as RQ
 from django.http import HttpResponseForbidden
 from rest_framework import status
+
 import json
+import requests
 """
     Rest Framework 을 위한 전용 커스텀 미들웨어에 대해 response format 을 자동으로 세팅
 """
@@ -16,9 +18,20 @@ class LogInMiddleware:
 
     def __call__(self, request):
         jwtToken = request.COOKIES[RQ.COOKIES_TOKEN]
+
+        # Request에서 쿠키를 받아 다시 재가공한 뒤 AUTH 서버에 유효성 검사를 진행
+        params = {'jwt': jwtToken}
+        response = requests.get(RQ.AUTH_URL, params=params)  # 요청 전송
+
+        # 잘못된 요청 진행
+        if response.status_code == 401:
+            return HttpResponseForbidden(status.HTTP_401_UNAUTHORIZED)
+
+        # Decode 진행
         decoded = converter.jwtTokenDecoder(jwtToken)
         if decoded is None:  # 잘못된 JWT 토큰이 들어올시 권한이 없다는 오류코드를 반환합니다.
             return HttpResponseForbidden(status.HTTP_401_UNAUTHORIZED)
+
         if not hasattr(request, 'uid'):
             request.uid = elements.getUid(
                 decoded['email'], decoded['provider'])
